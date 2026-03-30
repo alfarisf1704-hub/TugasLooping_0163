91ea62fda2d6a774e3079c57a5daa4836e1fe9ac
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-31 05:42:34 +0700

    membuat bool untuk rumjus primanya

--- PrimaNacci.cpp
+++ PrimaNacci.cpp
@@ -11,7 +11,14 @@ bool bilPrim(int n)
 {
     if (n <= 1)
         return false;
-    for (int i = 2; i <=)
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main()

1bcd3f85a691becf56f20336c5cfaec08e87083c
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-31 03:31:50 +0700

    revisi deklarasi sama prosedur biar rapih

--- PrimaNacci.cpp
+++ PrimaNacci.cpp
@@ -1,45 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int angkanya;
-    int pilihanopsi;
-    string statusAngka;
+// Deklarasi Variabel global
+int angkanya;
+int pilihanopsi;
+string statusAngka;
 
+// prosedur input
+void input()
+{
+    cout << "==== PENGECEKAN ANGKA =====" << endl;
+    cout << "=== Mau Ngecek Apa Nih? ===" << endl;
+    cout << " 1. Mengecek Bilangan Prima " << endl;
+    cout << " 2. Mengecek Bilangan Fibonacci" << endl;
+    cout << " 0. Keluar" << endl;
+    cout << "===========================" << endl;
+    cout << "Masukkan Pilihan Anda (0,1,2) : ";
+    cin >> pilihanopsi;
+}
+
+void pengecekan()
+{
     while (true)
     {
-        cout << "==== PENGECEKAN ANGKA =====" << endl;
-        cout << "=== Mau Ngecek Apa Nih? ===" << endl;
-        cout << " 1. Mengecek Bilangan Prima " << endl;
-        cout << " 2. Mengecek Bilangan Fibonacci" << endl;
-        cout << " 0. Keluar" << endl;
-        cout << "===========================" << endl;
-        cout << "Masukkan Pilihan Anda (0,1,2) : ";
-        cin >> pilihanopsi;
-
-        if (pilihanopsi == 1)
+        switch (pilihanopsi)
         {
-            cout << "Masukkan angka : ";
-            cin >> angkanya;
-            break;
-        }
-
-        else if (pilihanopsi == 2)
-        {
-            cout << "Masukkan angka : ";
-            cin >> angkanya;
-            break;
-        }
-
-        else if (pilihanopsi == 0)
-        {
-            return 0;
-        }
-
-        else
-        {
-            cout << "PILIHAN TIDAK VALID";
         }
     }
+}
+
+void output()
+{
 }

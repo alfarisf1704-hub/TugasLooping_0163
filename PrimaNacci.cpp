10ff0c3dcd381a72e51ba039b13345d2a5518006
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-30 21:30:34 +0700

    membuat prosedur input, while

--- PrimaNacci.cpp
+++ PrimaNacci.cpp
@@ -3,3 +3,15 @@ using namespace std;
 // Deklarasi Variabel Global
 int angkanya;
 string statusAngka;
+// prosedur input
+void input()
+{
+    while (true)
+    {
+        cout << "==== PENGECEKAN ANGKA =====" << endl;
+        cout << "=== Mau Ngecek Apa Nih? ===" << endl;
+        cout << " 1. Mengecek Bilangan Prima " << endl;
+        cout << " 2. Mengecek Bilangan Fibonacci" << endl;
+        cout << " 0. Keluar" << endl;
+    }
+}

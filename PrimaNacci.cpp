3a9d341fd5ed90443820183c969748a2cae545c3
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-31 05:10:22 +0700

    fiks yg dipakai

--- PrimaNacci.cpp
+++ PrimaNacci.cpp
@@ -12,10 +12,12 @@ int main()
     while (true)
     {
         cout << "==== PENGECEKAN ANGKA =====" << endl;
-        cout << "=== Mau Ngecek Apa Nih? ===" << endl;
-        cout << " 1. Mengecek Bilangan Prima " << endl;
+        cout << "=== Mau Ngecek Apa Nih? ===\n"
+             << endl;
+        cout << " 1. Mengecek Bilangan Prima" << endl;
         cout << " 2. Mengecek Bilangan Fibonacci" << endl;
-        cout << " 0. Keluar" << endl;
+        cout << " 0. Keluar\n"
+             << endl;
         cout << "===========================" << endl;
         cout << "Masukkan Pilihan Anda (0/1/2) : ";
         cin >> pilihanopsi;
@@ -23,7 +25,7 @@ int main()
         switch (pilihanopsi)
         {
         case 0:
-            return;
+            return 0;
             break;
         case 1:
             cout << "Masukkan angka : ";
@@ -35,9 +37,8 @@ int main()
             break;
         default:
             cout << endl;
-            cout << "PILIHAN TIDAK VALID";
+            cout << "PILIHAN TIDAK VALID\n";
             cout << endl;
-            break;
         }
     }
 }

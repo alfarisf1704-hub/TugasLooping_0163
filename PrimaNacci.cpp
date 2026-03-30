cbdb40ef7061f16c02d946f747020a1b74571ac9
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-30 21:28:45 +0700

    first commit

---
+++ PrimaNacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Deklarasi Variabel Global
+int angkanya;
+string statusAngka;
+// prosedur input
+void input()
+{
+    while (true)
+    {
+        cout << "==== PENGECEKAN ANGKA =====" << endl;
+        cout << "=== Mau Ngecek Apa Nih? ===" << endl;
+        cout << " 1. Mengecek Bilangan Prima " << endl;
+        cout << " 2. Mengecek Bilangan Fibonacci" << endl;
+        cout << " 0. Keluar" << endl;
+    }
+}

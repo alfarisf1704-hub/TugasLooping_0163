541d9ac09c575f6e3a5fc4f039292a7bc9a8625e
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-30 21:33:52 +0700

    menambahkan cin buat inputnya

--- PrimaNacci.cpp
+++ PrimaNacci.cpp
@@ -13,5 +13,8 @@ void input()
         cout << " 1. Mengecek Bilangan Prima " << endl;
         cout << " 2. Mengecek Bilangan Fibonacci" << endl;
         cout << " 0. Keluar" << endl;
+        cout << "===========================" << endl;
+        cout << "Masukkan Pilihan Anda (0,1,2) : " << endl;
+        cin >> angkanya;
     }
 }

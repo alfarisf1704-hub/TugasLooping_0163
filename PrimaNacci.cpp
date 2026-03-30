80cc4a62266fcc7943f073f1cb4f543aa436416a
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-31 03:38:10 +0700

    melengkapi void pengecekan

--- PrimaNacci.cpp
+++ PrimaNacci.cpp
@@ -15,7 +15,7 @@ void input()
     cout << " 2. Mengecek Bilangan Fibonacci" << endl;
     cout << " 0. Keluar" << endl;
     cout << "===========================" << endl;
-    cout << "Masukkan Pilihan Anda (0,1,2) : ";
+    cout << "Masukkan Pilihan Anda (0/1/2) : ";
     cin >> pilihanopsi;
 }
 
@@ -25,6 +25,22 @@ void pengecekan()
     {
         switch (pilihanopsi)
         {
+        case 0:
+            return;
+            break;
+        case 1:
+            cout << "Masukkan angka : ";
+            cin >> angkanya;
+            break;
+        case 2:
+            cout << "Masukkan angka : ";
+            cin >> angkanya;
+            break;
+        default:
+            cout << endl;
+            cout << "PILIHAN TIDAK VALID";
+            cout << endl;
+            break;
         }
     }
 }
